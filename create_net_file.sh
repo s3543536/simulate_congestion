53ff3c223263d94deb589d75481da6ec6f28a6c3
1wsx10 <1wsx10@gmail.com>
2017-09-04 17:20:32 +1000

    added map with just flinders street, a german road type file (TODO replace this)

---
+++ create_net_file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "$0 <type_file> <osm_file> <output_file>"
+echo "<type_file> (.typ.xml)"
+echo "<osm_file> (.osm)"
+echo "<output_file> (.net.xml)"
+
+netconvert --xml-type-files $1 --guess-ramps --remove-edges.by-vclass hov,taxi,bus,delivery,transport,lightrail,cityrail,rail_slow,rail_fast,motorcycle,bicycle,pedestrian --geometry.remove --remove-edges.isolated --try-join-tls --verbose --osm-files $2 --output-file $3
