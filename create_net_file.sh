58210786a53f4e774c80c9e63497cae080e55207
1wsx10 <1wsx10@gmail.com>
2017-09-05 17:16:33 +1000

    can't get the 'rou' file to generate, use the flinders directory

--- create_net_file.sh
+++ create_net_file.sh
@@ -5,4 +5,4 @@ echo "<type_file> (.typ.xml)"
 echo "<osm_file> (.osm)"
 echo "<output_file> (.net.xml)"
 
-netconvert --xml-type-files $1 --guess-ramps --remove-edges.by-vclass hov,taxi,bus,delivery,transport,lightrail,cityrail,rail_slow,rail_fast,motorcycle,bicycle,pedestrian --geometry.remove --remove-edges.isolated --try-join-tls --verbose --osm-files $2 --output-file $3
+netconvert --lefthand --xml-type-files $1 --guess-ramps --remove-edges.by-vclass hov,taxi,bus,delivery,transport,lightrail,cityrail,rail_slow,rail_fast,motorcycle,bicycle,pedestrian --geometry.remove --remove-edges.isolated --try-join-tls --verbose --osm-files $2 --output-file $3
