7599eebcddca3d6619fc1e63527093ff1afff9b5
1wsx10 <1wsx10@gmail.com>
2017-09-05 17:35:31 +1000

    created script to generate routes and improved create_net_file

--- create_net_file.sh
+++ create_net_file.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo "$0 <type_file> <osm_file> <output_file>"
-echo "<type_file> (.typ.xml)"
-echo "<osm_file> (.osm)"
-echo "<output_file> (.net.xml)"
+if [ "$#" -ne 1 ]; then
+	echo "$0 <type_file> <osm_file> <output_file>"
+	echo "<type_file> (.typ.xml)"
+	echo "<osm_file> (.osm)"
+	echo "<output_file> (.net.xml)"
+	exit
+fi
 
 netconvert --lefthand --xml-type-files $1 --guess-ramps --remove-edges.by-vclass hov,taxi,bus,delivery,transport,lightrail,cityrail,rail_slow,rail_fast,motorcycle,bicycle,pedestrian --geometry.remove --remove-edges.isolated --try-join-tls --verbose --osm-files $2 --output-file $3
